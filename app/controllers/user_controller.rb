48cc205a84c2427afbed9fb8db48c3da95495296
Alex Lee <hyunho98@gmail.com>
2020-03-15 18:43:27 +0000

    added login/signup post methods

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+
+  post '/login' do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/index'
+    else
+      redirect '/'
+    end
+  end
+
   get '/signup' do
     erb :signup
+  end
+
+  post '/signup' do
+    user = User.new(params)
+    if user.save && user.name != "" && user.email != "" && user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/index'
+    else
+      redirect '/signup'
+    end
   end
 end
