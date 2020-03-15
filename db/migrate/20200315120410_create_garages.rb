d406cd8f32f79fe2b978dd7d557ec5a9f113fec6
Alex Lee <hyunho98@gmail.com>
2020-03-15 17:03:14 +0000

    created and migrated tables

--- db/migrate/20200315120410_create_garages.rb
+++ db/migrate/20200315120410_create_garages.rb
@@ -1,4 +1,9 @@
 class CreateGarages < ActiveRecord::Migration
   def change
+    create_table :garages do |t|
+      t.string :name
+      t.string :address
+      t.integer :capacity
+    end
   end
 end
--- db/migrate/20200315120451_create_cars.rb
+++ db/migrate/20200315120451_create_cars.rb
@@ -1,4 +1,10 @@
 class CreateCars < ActiveRecord::Migration
   def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.integer :year
+      t.float :price
+    end
   end
 end
