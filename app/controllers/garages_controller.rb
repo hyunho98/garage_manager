c4bbb9047fbb52c2e1dcc4e6e0f643c6eeb93cf4
Alex Lee <hyunho98@gmail.com>
2020-03-15 19:37:15 +0000

    added garages get method and garages index

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,2 +1,8 @@
 class GaragesController < ApplicationController
+  get '/garages' do
+    not_logged_in?
+    @user = current_user
+    @garages = @user.garages
+    erb :'garages/index'
+  end
 end
