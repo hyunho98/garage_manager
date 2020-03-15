8d36b5bafa6fa2ebd90bdb26ee21a6e52f70e3f9
Alex Lee <hyunho98@gmail.com>
2020-03-15 18:30:19 +0000

    updated signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, uniqueness: true
   validates :email, presence: true
-  validates :username, uniqueness: true
-  validates :username, presence: true
   has_many :garages
   has_many :cars
 end
