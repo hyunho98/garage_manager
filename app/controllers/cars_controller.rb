7aba93cb8e9b25a17b9e3c3be7160dad6fc6ecc3
Alex Lee <hyunho98@gmail.com>
2020-03-16 04:17:39 +0000

    added patch method

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -36,14 +36,26 @@ class CarsController < ApplicationController
   post '/cars' do
     not_logged_in?
     if !params[:year].empty? && !params[:make].empty? && !params[:model].empty? && !params[:type].empty? && !params[:license_plate].empty? && !params[:price].empty?
-      car = Car.new(year: params[:year].to_i, make: params[:make], model: params[:model], type: params[:type], license_plate: params[:license_plate], price: params[:price].to_f)
-      if car.save && !params[:garage].empty?
+      car = Car.new(year: params[:year].to_i, make: params[:make], model: params[:model], type: params[:type], license_plate: params[:license_plate], price: params[:price].to_f, garage_id: params[:garage_id])
+      if car.save
         current_user.cars << car
         current_user.garages.cars << car
         redirect "/cars/#{car.id}"
       else
         redirect "/cars/new"
       end
+    end
+  end
+
+  patch '/cars/:id' do
+    not_logged_in?
+    if !params.keys.include?("garage_id")
+      params[:garage_id] = nil
+    end
+
+    @car = Car.find(params[:id])
+    @car.update(params)
+    redirect "/garages/#{@garage.id}"
     end
   end
 
