ff82de7fa7ddd23841e11894ce20728e01f42c40
Alex Lee <hyunho98@gmail.com>
2020-03-16 03:10:46 +0000

    added '/cars/:id' method and cars show page

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,4 +13,12 @@ class CarsController < ApplicationController
     erb :'/cars/index'
   end
 
+  get '/cars/:id' do
+    not_logged_in?
+    @car = Car.find(params[:id])
+    belong_here?(@car.user.id)
+    @garage = Garage.find(@car.garage_id)
+    erb :'/cars/show'
+  end
+
 end
